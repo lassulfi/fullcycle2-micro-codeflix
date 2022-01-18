31198b6ed9407064631e894c350ba359ee1f3185
Luis Daniel Assulfi <lassulfi@gmail.com>
2022-01-17 21:48:14 -0300

    Criando componente de slider personalizado

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,25 @@
+import { makeStyles } from "@material-ui/core";
+import clsx from "clsx";
+import ReactSlider, { Settings } from "react-slick";
+
+const useStyles = makeStyles({
+    root: {
+        "&:hover .slick-left, &:hover .slick-right": {
+            opacity: 1,
+        },
+    },
+});
+
+export interface SliderProps extends Settings {
+    
+}
+ 
+const Slider: React.FunctionComponent<SliderProps> = (props) => {
+    const { children, ...other } = props;
+    const classes = useStyles()
+    const className = clsx(classes.root, other.className);
+
+    return <ReactSlider className={className} {...other}>{children}</ReactSlider>;
+}
+ 
+export default Slider;
